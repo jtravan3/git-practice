0a4455803dd09af101c5a7f586f728004fd8fbc0
cybot-des <gobunny2020@gmail.com>
2021-06-24 17:59:38 +0530

    practice to add a java program

---
+++ forest/Tiger.java
@@ -0,0 +1,10 @@
+package forest;
+import java.util.*;
+public class Tiger
+{
+public void getDetails(String nickName, int weight)
+{
+System.out.println("Tiger nick name is "+nickName);
+System.out.println("Tiger weight is "+weight);
+}
+}
